63069a8fdc52464f2e2183d09957857736154a6c
Maksym Blinov <m.blinov@it-sprout.org>
2023-04-21 18:46:37 +0300

    build

---
+++ terraform-azure/output.tf
@@ -0,0 +1,12 @@
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+
+  sensitive = true
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.aks-rg.name
+}
+output "wordpress_url" {
+  value = "https://${azurerm_dns_cname_record.aks-dns-zone.record}"
+}
---
+++ terraform-azure/scripts/setup-wp.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+mysql -h $SERVER_NAME -u $USER_NAME -p$PASSWORD -P 3306 -e "\
+   CREATE DATABASE IF NOT EXISTS test_manifest12; \
+    USE test_manifest12; \
+    CREATE TABLE IF NOT EXISTS my_table (id INT PRIMARY KEY, name VARCHAR(50)); \
+    INSERT IGNORE INTO my_table (id, name) VALUES (1, 'John'), (2, 'Jane'), (3, 'Joe');" 
+if [  -f /var/www/html/wp-config.php ]; then  
+    wp --allow-root --path=/var/www/html plugin install contact-form-7 --activate && \
+    wp --allow-root --path=/var/www/html theme install hestia    && \
+    wp --allow-root --path=/var/www/html theme activate hestia     
+else    
+    # Configure WP-CLI and install plugins and themes
+    wp core download --path=/var/www/html --allow-root && \
+    wp --allow-root --path=/var/www/html config create \
+      --dbhost=mysql-wpmax.mysql.database.azure.com \
+      --dbname=test_manifest12 \
+      --dbuser=mysqladmin@mysql-wpmax \
+      --dbpass=1-qwerty \
+      --allow-root && \
+    wp --allow-root --path=/var/www/html core install \
+      --url=$(terraform output wordpress_url)  \
+      --title=WPsite \
+      --admin_user=admin \
+      --admin_password=admin \
+      --admin_email=admin@example.com && \
+    wp --allow-root --path=/var/www/html plugin install contact-form-7 --activate && \
+    wp --allow-root --path=/var/www/html theme install twentytwentytwo  && \
+    wp --allow-root --path=/var/www/html theme activate twentytwentytwo     
+fi
+
+
+
+
+
