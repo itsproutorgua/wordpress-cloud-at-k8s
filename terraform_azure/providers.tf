3956507c38732ae54f1cf73534bb1d84b5a47d9f
dmonakh <d.monakh@it-sprout.org>
2023-06-15 11:57:18 +0300

    init

--- terraform_azure/providers.tf
+++ terraform_azure/providers.tf
@@ -15,7 +15,7 @@ terraform {
   }
     backend "azurerm" {
     resource_group_name  = "tfstate"
-    storage_account_name = "tfstatemondy123"
+    storage_account_name = "tfstatemondy2023"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     }
