43bc90c1d00db676556f2efa02793a011b7fce60
dmonakh <d.monakh@it-sprout.org>
2023-06-21 16:42:57 +0300

    destroy

--- terraform_aws/backend.tf
+++ terraform_aws/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mondyk8sbucket1911"  
+    bucket         = "mondyk8sbucket1911"  # Replace with you bucket name.
     key            = "terraform.tfstate"   
     region         = "us-east-1"           
     encrypt        = true                  
